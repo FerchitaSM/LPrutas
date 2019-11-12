af39a7880ec655d669ccf1bb6f0f989fcec282f6
Carlos Cavero <54409608+ccaverotx@users.noreply.github.com>
2019-11-12 15:15:20 -0400

    fix
    "Correct the classpath of your application so that it contains a single, compatible version of org.springframework.beans.factory.annotation.AnnotatedBeanDefinition"

--- src/main/java/com/example/lp/LPrutasApplication.java
+++ src/main/java/com/example/lp/LPrutasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = { "com.example.lp.dao","com.example.lp.bot" })
-public class LPrutasApplication {
+public class  LPrutasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LPrutasApplication.class, args);
