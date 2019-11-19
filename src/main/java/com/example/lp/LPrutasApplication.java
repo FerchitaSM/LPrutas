fc965fd9a65fd74c47dde7be3d3287692ada6a44
Carlos Cavero <ccaverotx@gmail.com>
2019-11-19 18:07:06 -0400

    Project Fixed on Linux v:

--- src/main/java/com/example/lp/LPrutasApplication.java
+++ src/main/java/com/example/lp/LPrutasApplication.java
@@ -1,7 +1,10 @@
 package com.example.lp;
 
+import com.example.lp.bot.BotInicializator;
+import com.sun.istack.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@SpringBootApplication(scanBasePackages = { "com.example.lp.dao","com.example.lp.bot","com.example.lp.bl"})
 //@EnableJpaRepositories(basePackages = {"com.example.lp.domain", "com.example.lp.dao"})
